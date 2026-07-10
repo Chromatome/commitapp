ef9be6812a4b07e5f23fc93684e8e0e20c9ccd6b
miniaturity <miniaturity2.o@gmail.com>
2026-07-10 10:55:16 -0400

    test

---
+++ src/pages/Marketplace.tsx
@@ -0,0 +1,12 @@
+import '../styles/styles.css';
+
+const MarketPlace: React.FC = () => {
+  return (
+    <div className="marketplace-page">
+      <h1>Marketplace</h1>
+      <p>Welcome to the Marketplace page!</p>
+    </div>
+  );
+};
+
+export default MarketPlace;
