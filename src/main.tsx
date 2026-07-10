9a437b8066ecc765aa310f24443c6239febd8c74
miniaturity <miniaturity2.o@gmail.com>
2026-07-10 11:04:32 -0400

    chore: import tailwind

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,15 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import LandingPage from './pages/LandingPage.tsx'
 import { BrowserRouter, Route, Routes } from 'react-router'
+import MarketPlace from './pages/Marketplace.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<LandingPage />} />
+        <Route path="/marketplace" element={<MarketPlace />} />
+        
       </Routes>
     </BrowserRouter>
   </StrictMode>,
--- src/pages/Marketplace.tsx
+++ src/pages/Marketplace.tsx
@@ -3,8 +3,7 @@ import '../styles/styles.css';
 const MarketPlace: React.FC = () => {
   return (
     <div className="marketplace-page">
-      <h1>Marketplace</h1>
-      <p>Welcome to the Marketplace page!</p>
+      
     </div>
   );
 };
